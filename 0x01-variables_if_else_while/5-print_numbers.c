b127fa47db744a61d77cb140c760bdcfa6779020
JUMWA <siryajumwa@gmail.com>
2022-03-14 16:54:52 -0700

    loops

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *prints all single digits numbers that are of base 10
+ *starting from 0
+ */
+int main(void)
+{
+  char c = '0';
+  while (c <= '9')
+  {
+    putchar(c);
+    c++;
+  }
+  putchar('\n');
+  return (0);
+}
