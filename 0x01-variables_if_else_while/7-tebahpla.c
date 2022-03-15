213c22bdc5eadb8a9f5d99d640f8536fc50a7919
JUMWA <siryajumwa@gmail.com>
2022-03-14 17:06:31 -0700

    loops

---
+++ 0x01-variables_if_else_while/7-tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *prints all single digit number with a base of 10 that start from 0
+ */
+int main(void)
+{
+  char c = 'z';
+  while (c >= 'a')
+  {
+    putchar(c);
+    c--;
+  }
+  putchar('\n');
+  return (0);
+}
