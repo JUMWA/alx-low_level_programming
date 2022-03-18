4b4d3849426cdbf5fcab0bca7293c5bd13b29122
JUMWA <siryajumwa@gmail.com>
2022-03-18 05:38:15 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *main -test function for positive or negative
+ */
+int main(void)
+{
+int i;
+
+i=0;
+positive_or_negative(i);
+return (0);
+}
---
+++ 0x03-debugging/1.main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - causes an infinite loop
+ */
+
+int main(void)
+{
+int i;
+printf("Infinite loop incoming :(\n");
+i=0;
+printf("infinite loop avoided! \o/\n");
+Return (0);
+}
